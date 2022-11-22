74dd94fd7bae8592a6b4eaaa650f7d840d92af48
AayushParashar28 <ayushparashar6203@gmail.com>
2022-11-23 01:08:37 +0530

    updated server

--- newserver.js
+++ newserver.js
@@ -1,8 +1,8 @@
 function downloaadSong(songName, callback) {
-    
+
     console.log("Downloading started .....");
 
-    setTimeout(function fun(){
+    setTimeout(function fun() {
 
         let song = songName + ".mp3";
         callback(song);
@@ -12,57 +12,59 @@ function downloaadSong(songName, callback) {
 }
 
 let song = downloaadSong("excuses", function x(song) {
-    
+
     console.log("Song Downloading completed");
     console.log("your song ", song);
 
-    function songfile(songfile, callback) {
-    
-        setTimeout(function fun(){
-         
-            console.log("File Started .....");
-           
-        }, 4000);
-    
-        setTimeout(function fun(){
-    
-            let file1 = songfile + ".txt";
-            callback(file1);
-    
-        }, 5000);
-    
-    }
-
     let file = songfile(song, function x(song) {
-    
+
         console.log("File Completed");
         console.log("File name is ", song);
 
-        function drivelink(songdrive, callback) {
-    
-            setTimeout(function fun(){
-             
-                console.log("Drive Started .....");
-               
-            }, 6000);
-        
-            setTimeout(function fun(){
-        
-                let drive1 = "www.drive.google.com/" + songdrive ;
-                callback(drive1);
-        
-            }, 7000);
-        
-        }
-        
-        let drive = drivelink(song , function x(song) {
-            
+        let drive = drivelink(song, function x(song) {
+
             console.log("link open done");
             console.log("Your Drive link is ", song);
-        
-        });
     
+        });
     });
 
 });
+
+
+function songfile(songfile, callback) {
+
+    setTimeout(function fun() {
+
+        console.log("File Started .....");
+
+    }, 4000);
+
+    setTimeout(function fun() {
+
+        let file1 = songfile + ".txt";
+        callback(file1);
+
+    }, 5000);
+
+}
+
+
+function drivelink(songdrive, callback) {
+
+    setTimeout(function fun() {
+
+        console.log("Drive Started .....");
+
+    }, 6000);
+
+    setTimeout(function fun() {
+
+        let drive1 = "www.drive.google.com/" + songdrive;
+        callback(drive1);
+
+    }, 7000);
+
+}
+
 
